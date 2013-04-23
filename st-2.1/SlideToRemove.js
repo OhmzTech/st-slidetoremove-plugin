3830927c4047361b1021653467a23df777e0c982
OhmzTech <OhmzTech@android_a100000d9e9ee6.gateway.2wire.net>
2013-04-23 18:01:47 -0500

    Comment clean-up

--- st-2.1/SlideToRemove.js
+++ st-2.1/SlideToRemove.js
@@ -3,16 +3,7 @@ Plugin: Ext.plugin.SlideToRemove
 Version: 1.2.0
 Tested: Sencha Touch 2.1
 Author: OhmzTech (www.ohmztech.com)
-
-Usage Notes:
-1. Add this plugin to any list.
-2. User can now swipe the list item to display delete button, and swipe it back to hide the delete button. Multiple delete buttons can be open at one time.
-3. Set buttonText in plugin configuration to change text on the delete button, and buttonWidth to change the width of the button.
-4. Easily change the button configuration manually in this plugin for advanced customization.
-5. By default the handler for the button will remove the record from the store.
-6. The closeDeletes method will close all open delete buttons.
 */
-
 
 Ext.define('Ext.plugin.SlideToRemove', {
     extend: 'Ext.Component',
